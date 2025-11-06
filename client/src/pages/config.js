b95ce0ad251f47289008e27c8fef7b531c0cd962
kattasivanand <kattasivanand123@gmail.com>
2025-11-06 22:44:45 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = "http://3.80.67.98:86";
-//const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
+//const API_BASE_URL = "http://3.80.67.98:86";
+const API_BASE_URL = "http://adc4480321f1341019be59527eda9590-1262517554.ap-south-1.elb.amazonaws.com";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 /// export default API_BASE_URL;
