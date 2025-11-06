e1d5632911f3314789c28dee4e75da1549e9f898
kattasivanand <kattasivanand123@gmail.com>
2025-11-06 21:33:26 +0530

    Update vpc.tf

--- terraform_main_ec2/vpc.tf
+++ terraform_main_ec2/vpc.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
@@ -19,25 +19,28 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public-subnet1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "${var.region}a"
   map_public_ip_on_launch = true
 
   tags = {
     Name = var.subnet-name1
   }
 }
+
 resource "aws_subnet" "public-subnet2" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.0.0/24"
-  availability_zone       = "us-east-1b"
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "${var.region}b"
   map_public_ip_on_launch = true
 
   tags = {
     Name = var.subnet-name2
   }
 }
+
 resource "aws_route_table" "rt" {
   vpc_id = aws_vpc.vpc.id
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
@@ -48,39 +51,47 @@ resource "aws_route_table" "rt" {
   }
 }
 
-resource "aws_route_table_association" "rt-association" {
-  route_table_id = aws_route_table.rt.id
+resource "aws_route_table_association" "rt-association1" {
   subnet_id      = aws_subnet.public-subnet1.id
-}
-resource "aws_route_table_association" "rt-association2" {
   route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_route_table_association" "rt-association2" {
   subnet_id      = aws_subnet.public-subnet2.id
+  route_table_id = aws_route_table.rt.id
 }
 
 resource "aws_security_group" "security-group" {
   vpc_id      = aws_vpc.vpc.id
-  description = "Allowing Jenkins, Sonarqube, SSH Access"
+  description = "Allowing Jenkins, Sonarqube, SSH, HTTP, MySQL Access"
 
   ingress = [
     for port in [22, 8080, 9000, 9090, 3306, 80] : {
-      description      = "TLS from VPC"
+      description      = "Inbound port ${port}"
       from_port        = port
       to_port          = port
       protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
-      self             = false
       prefix_list_ids  = []
       security_groups  = []
-      cidr_blocks      = ["0.0.0.0/0"]
+      self             = false
     }
   ]
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+      description      = "Allow all outbound traffic"
+    }
+  ]
 
   tags = {
     Name = var.sg-name
