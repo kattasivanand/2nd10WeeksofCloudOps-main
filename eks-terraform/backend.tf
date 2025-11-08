5c0fc6ed7c8057908274f44c5690f5f37a57af9d
kattasivanand <kattasivanand123@gmail.com>
2025-11-08 13:19:17 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_providers {
-    aws = {
-        source = "hashicorp/aws"
-        version = ">= 5.25.0"
-    }
+  backend "s3" {
+    bucket = "my-eks-terraform-state-12345"   # Replace with your unique bucket name
+    key    = "eks/pipeline/terraform.tfstate" # Path for the state file
+    region = "ap-south-1"                     # Region where bucket exists
   }
 }
+
