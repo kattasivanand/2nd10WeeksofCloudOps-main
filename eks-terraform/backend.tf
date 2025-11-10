4aad9fee9ecb3e8a21c141ab756b140b7059213c
kattasivanand <kattasivanand123@gmail.com>
2025-11-10 20:20:26 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mnbvcxzkjhgfdsaqwertyukjhgfdszxcvbnm"   # Replace with your unique bucket name
+    bucket = "my-eks-terraform-state-12345"   # Replace with your unique bucket name
     key    = "eks/pipeline/terraform.tfstate" # Path for the state file
     region = "ap-south-1"                     # Region where bucket exists
   }
